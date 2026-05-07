87fe3c41384fae16ae86cf2d8fac86b509cc648d
NsdHSO <nechiforelsamuel@gmail.com>
2026-05-07 03:11:41 +0300

    chore: update eslint-config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,38 +1,47 @@
-const {defineConfig} = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const unusedImports = require("eslint-plugin-unused-imports");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const unusedImports = require('eslint-plugin-unused-imports');
 
 module.exports = defineConfig([
-    ...expoConfig,
-    {
-        ignores: ["dist/*", "node_modules/*"],
+  ...expoConfig,
+  {
+    ignores: ['dist/*', 'node_modules/*'],
+  },
+  {
+    plugins: {
+      'unused-imports': unusedImports,
     },
-    {
-        plugins: {
-            "unused-imports": unusedImports,
+    rules: {
+      // Disable import/no-unresolved for workspace packages - TypeScript handles this
+      'import/no-unresolved': [
+        'error',
+        {
+          ignore: ['^@dental/'],
         },
-        rules: {
-            // Disable import/no-unresolved for workspace packages - TypeScript handles this
-            "import/no-unresolved": ["error", {
-                ignore: ["^@yuhuu/"]
-            }],
-            "@typescript-eslint/no-unused-vars": "off",
-            "unused-imports/no-unused-imports": "error",
-            "unused-imports/no-unused-vars": [
-                "warn",
-                {
-                    vars: "all",
-                    varsIgnorePattern: "^_",
-                    args: "after-used",
-                    argsIgnorePattern: "^_",
-                },
-            ],
+      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
         },
+      ],
     },
-    {
-        files: ["**/__tests__/**/*.{ts,tsx,js,jsx}", "**/*.test.{ts,tsx,js,jsx}", "**/*.spec.{ts,tsx,js,jsx}", "**/jest.setup.{ts,js}", "**/__mocks__/**/*.{ts,tsx,js,jsx}"],
-        rules: {
-            "@typescript-eslint/no-require-imports": "off",
-        },
+  },
+  {
+    files: [
+      '**/__tests__/**/*.{ts,tsx,js,jsx}',
+      '**/*.test.{ts,tsx,js,jsx}',
+      '**/*.spec.{ts,tsx,js,jsx}',
+      '**/jest.setup.{ts,js}',
+      '**/__mocks__/**/*.{ts,tsx,js,jsx}',
+    ],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
     },
+  },
 ]);
